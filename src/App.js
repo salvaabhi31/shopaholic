3366f1b88e437d37fa87d1cd88075a949a2c65d1
EC2 Default User <ec2-user@ip-172-31-9-105.us-east-2.compute.internal>
2020-08-16 15:12:20 +0000

    updating our app with shop-page,collection-preview&item component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,17 @@
 import React from 'react';
 import { Route,Switch } from 'react-router-dom'
 import Homepage from './pages/homepage/homepage.component'
+import ShopPage from './pages/shop/shop.component.jsx';
 import './App.css';
 
-const HatsPage=()=>(
-  <div>
-  <h1>Hats</h1>
-  </div>
-  )
+
 
 function App() {
   return (
     <div>
     <Switch>
     <Route exact path='/' component={Homepage}/>
-    <Route  path='/Hats' component={HatsPage}/>
+    <Route  path='/shop' component={ShopPage}/>
      </Switch>
     </div>
   );
