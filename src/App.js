cf5d0f0a2e72f7e263494edf5d273febef712c14
EC2 Default User <ec2-user@ip-172-31-9-105.us-east-2.compute.internal>
2020-08-22 10:00:06 +0000

    created sign-up page usig firestore

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Homepage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shop/shop.component.jsx';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx';
 import Header from './components/header/header.component.jsx'
-import { auth } from './firebase/firebase.utils';
+import { auth,createUserProfileDocument } from './firebase/firebase.utils';
 
 import './App.css';
 
@@ -22,12 +22,37 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user });
-
-      console.log(user);
-    });
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+     
+      if(userAuth)
+      {
+        const userRef= await createUserProfileDocument(userAuth);
+        userRef.onSnapshot(snapShot=>{
+           this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          
+        
+        
+        
+      });
+      
+    })
+      }
+    
+    else
+    {
+      this.setState({ currentUser: userAuth }); 
+    }
+    
+     
+    
+  });
   }
+  
+  
 
   componentWillUnmount() {
     this.unsubscribeFromAuth();
--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -2,10 +2,14 @@ import React from 'react';
 
 import './sign-in-and-sign-up.styles.scss'
 import SignIn from'../../components/signin/signin.component.jsx'
+import SignUp from '../../components/sign-up/sign-up.component';
 
 const SignInAndSignUpPage =()=>(
     
-    <div className="sign-in-and-sign-up"><SignIn /></div>
+    <div className="sign-in-and-sign-up">
+    <SignIn />
+    <SignUp />
+    </div>
     
     );
     
