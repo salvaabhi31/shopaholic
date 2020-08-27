33e8b4417866a8ffaf2f41ae878b459d619a9382
EC2 Default User <ec2-user@ip-172-31-9-105.us-east-2.compute.internal>
2020-08-27 06:24:42 +0000

    added cart-icon,cart-dropdown component. added their redux functionalities

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 import { connect } from 'react-redux';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
+import CartIcon from '../cart-icon/cart-icon.component';
 import{ReactComponent as Logo } from "../../assets/crown.svg"
 import { auth } from '../../firebase/firebase.utils';
 
 import './header.styles.scss';
 
-const Header =({currentUser})=>(
+const Header =({currentUser,hidden})=>(
     
     <div className="header">
      <Link to="/" className="logo-container">
@@ -25,13 +27,16 @@ const Header =({currentUser})=>(
          :
          <Link className="option" to="/signin">SIGN-IN</Link>
      }
+     <CartIcon />
      </div>
+      {hidden ? null : <CartDropdown />}
     
     </div>
 )
 
-const mapStateToProps = state => ({
-  currentUser: state.user.currentUser
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
+  currentUser,
+  hidden
 });
 
 export default connect(mapStateToProps)(Header);
