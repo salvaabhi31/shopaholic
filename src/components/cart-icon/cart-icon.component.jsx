66c2e5e05766cd3a22b7389604bb056085d7d3f3
EC2 Default User <ec2-user@ip-172-31-9-105.us-east-2.compute.internal>
2020-08-28 10:05:14 +0000

    added our first selector

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,17 +1,17 @@
-import React from 'react'
-
+import React from 'react';
 import { connect } from 'react-redux';
 
-import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 import { toggleCartHidden } from '../../redux/cart/cart.actions';
+import { selectCartItemsCount } from '../../redux/cart/cart.selectors';
 
-import './cart-icon.styles.scss'
+import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 
+import './cart-icon.styles.scss';
 
-const CartIcon = ({ toggleCartHidden }) => (
-  <div className='cart-icon'>
-    <ShoppingIcon className='shopping-icon' onClick={toggleCartHidden} />
-    <span className='item-count'>0</span>
+const CartIcon = ({ toggleCartHidden, itemCount }) => (
+  <div className='cart-icon' onClick={toggleCartHidden}>
+    <ShoppingIcon className='shopping-icon' />
+    <span className='item-count'>{itemCount}</span>
   </div>
 );
 
@@ -19,7 +19,11 @@ const mapDispatchToProps = dispatch => ({
   toggleCartHidden: () => dispatch(toggleCartHidden())
 });
 
+const mapStateToProps = state => ({
+  itemCount: selectCartItemsCount(state)
+});
+
 export default connect(
-  null,
+  mapStateToProps,
   mapDispatchToProps
 )(CartIcon);
